607bb97b78192bdc1b6d2293e72ccc96168dfa4f
James0972 <108304105+James0972@users.noreply.github.com>
2024-01-08 23:37:36 +0200

    Add files via upload

---
+++ newapp/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    posts=Post.objects.all()
+    return render(request,'index.html', {'posts': posts})
+
+def post(request, pk):
+    posts=Post.objects.get(id=pk)
+
+    return render(request, 'post.html', {'posts': posts} )
+
+# Create your views here.
